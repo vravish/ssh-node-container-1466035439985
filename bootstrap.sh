fa1b1690e9cce7e6b1ec495875cda84ef6a036d1
vravish <vravish@us.ibm.com>
2016-06-21 17:53:58 -0400

    Changed bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,12 +9,12 @@ cp knife.rb chef-repo/.chef
 cp admin.pem chef-repo/.chef
 
 wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.15.15-1_amd64.deb
-dpkg -—install chefdk_0.15.15-1_amd64.deb
+dpkg --install chefdk_0.15.15-1_amd64.deb
 
 cd chef-repo
 knife ssl fetch
 knife ssl check
 
-knife bootstrap 169.44.119.171 --ssh-user vravish --ssh-password 'welcome' --sudo --use-sudo-password --node-name node1 --run-list 'recipe[venu_tomcat]'
+knife bootstrap ${FLOATING_IP} --ssh-user vravish --ssh-password 'welcome' --sudo --use-sudo-password --node-name node1 --run-list 'recipe[venu_tomcat]'
 
 
