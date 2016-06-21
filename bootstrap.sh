edbff1b6ea7d79cc2d01c6639059cc034b369806
vravish <vravish@us.ibm.com>
2016-06-21 17:33:31 -0400

    Including knife files and updated bootstrap.sh to try more commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,16 @@ echo $IC_COMMAND is the command to call for ice
 
 $IC_COMMAND exec -it $MY_CONTAINER_NAME /usr/sbin/sshd
 
+mkdir -p chef-repo/.chef
+cp knife.rb chef-repo/.chef
+cp admin.pem chef-repo/.chef
 
+wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.15.15-1_amd64.deb
+dpkg -—install chefdk_0.15.15-1_amd64.deb
+
+cd chef-repo
+knife ssl fetch
+knife ssl check
 
 knife bootstrap 169.44.119.171 --ssh-user vravish --ssh-password 'welcome' --sudo --use-sudo-password --node-name node1 --run-list 'recipe[venu_tomcat]'
 
