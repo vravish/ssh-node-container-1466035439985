6d40773ab0276adc684511310d8803f3bd602079
vravish <vravish@us.ibm.com>
2016-07-15 16:34:21 -0400

    Adding support for WAS liberty and passwordless sudo

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,14 @@ FROM mysql:5.7
 # Create app directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-
-# Install app dependencies
-COPY package.json /usr/src/app/
-# RUN npm install
-
-# Bundle app source
 COPY . /usr/src/app
 
 # Set up SSH
 RUN apt-get update ; apt-get install -y zip
 RUN apt-get install -y sudo openssh-server
-RUN mkdir /var/run/sshd
+RUN sed -i '/^.sudo/c\%sudo ALL=(ALL) NOPASSWD:ALL' /etc/sudoersRUN mkdir /var/run/sshd
 RUN chmod 0755 /var/run/sshd
-RUN useradd --create-home --shell /bin/bash --groups sudo vravish
+RUN useradd --create-home --shell /bin/bash --groups sudo,admin vravish
 RUN printf "welcome\nwelcome\n" | passwd vravish
 
 # Add public key to right place
@@ -38,5 +32,3 @@ EXPOSE 80
 EXPOSE 9080
 EXPOSE 1111
 EXPOSE 3306
-
-# CMD ["npm", "start"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ echo $NODE_IP is the IP of the node container
 
 # Activate SSH and enable passwordless login for this VM
 $IC_COMMAND exec -it $CONTAINER_ID /usr/sbin/sshd
-$IC_COMMAND exec -it $CONTAINER_ID apt-get install -y default-jre curl
+$IC_COMMAND exec -it $CONTAINER_ID apt-get install -y default-jre curl wget
 mkdir -p ~/.ssh
 cp ./id_rsa* ~/.ssh
 chmod 0700 ~/.ssh/id_rsa*
@@ -23,7 +23,7 @@ ssh -o StrictHostKeyChecking=no vravish@$NODE_IP 'echo Successfully accessed nod
 mkdir -p chef-repo/.chef
 cp knife.rb chef-repo/.chef
 cp admin.pem chef-repo/.chef
-cp -r cookbooks chef-repo
+cp -r roles chef-repo
 
 wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.15.15-1_amd64.deb
 sudo dpkg --install chefdk_0.15.15-1_amd64.deb
@@ -33,17 +33,10 @@ knife ssl fetch
 knife ssl check
 
 
-# Upload cookbooks from here
-#knife cookbook site install mysql
-#knife cookbook upload venu_tomcat -—include-dependencies
-#knife cookbook upload venu_mysql —-include-dependencies
-
-
 # Make sure vravish can change all items in the home folder
 ssh vravish@$NODE_IP 'sudo chown -R vravish:vravish /home/vravish'
 
 
 # Finally do the knife bootstrapping
-knife bootstrap $NODE_IP -y --ssh-user vravish --ssh-password 'welcome' --sudo --use-sudo-password --node-name tomcat-and-mysql --run-list 'recipe[venu_tomcat]' || echo
-
+knife bootstrap $NODE_IP --yes --node-name liberty --ssh-identity-file ~/.ssh/id_rsa -ssh-user vravish --run-list 'role[was_liberty_v8558_role]' --sudo
 
